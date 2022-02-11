d561b969ca9fa67547836f5aa524839924069544
Aaliya Farhin <91542100+aaliyafarhin@users.noreply.github.com>
2022-02-11 15:57:41 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,8 +2,8 @@ export const aboutData = {
     title: "Little about me",
     description1:"🏡 I live in Chennai, TamilNadu-IN.",
     description2:" 👨‍👨‍👦‍👦 I belong to a family of three.",
-    description3:"😍 I like watching amazing art, listening songs and reading self help books.",
-    description4:"🤗 When i'm free; I like to explore startups, watch ted talks, and talk with some good vibing humans.",
-    description5:"❤️️ I love to swim and also binge watch amazing suspenseful and sci-fi web series and movies.",
+    description3:"😍 I like Morning walk to beach, I believe mediation on the beach at twilight is the perfect start for the day",
+    description4:"🤗 When i'm free; I like to explore startups, watch ted talks, and talk with some good vibing humans .",
+    description5:"❤️️ I love to eat different cuisines, that love towards food made me a great cook.",
     image: 1
 }
