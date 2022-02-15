1ba7437ede6a1f06aedc4f1508f9f35b3d129906
Aaliya Farhin <91542100+aaliyafarhin@users.noreply.github.com>
2022-02-15 10:14:47 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,7 +1,7 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: "AaliyaFarhin",
+    name: "Aaliya Farhin",
     title: "👋 Hey... I'am",
     desciption:"An Aspiring Full Stack Web Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs / MongoDb and some other cool libraries and frameworks.",
     image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
